c4b23f35137f10b5e805034ced5ac5d70caa94b3
Marcos Fernández <marcos.fersu@gmail.com>
2023-11-01 19:50:28 -0300

    fix light

--- src/Light.jsx
+++ src/Light.jsx
@@ -1,26 +1,30 @@
 import { useGLTF } from "@react-three/drei";
-import { useControls } from "leva";
 
 const Light = () => {
-	const { nodes } = useGLTF("./model/pirate.glb");
-	// const { colorLight } = useControls("light", {
-	// 	colorLight: "#FFEEA8",
-	// });
-	return (
-		<>
-			{/* LIGHT */}
-			<mesh
-				geometry={nodes.bakedLight.geometry}
-				scale={0.2}
-				position={[0.42, -0.585, -1.655]}
-			>
-				<meshBasicMaterial
-					color={[2.5 * 10, 2 * 10, 1 * 10]}
-					toneMapped={false}
-				/>
-			</mesh>
-		</>
-	);
+  const { nodes } = useGLTF("./model/pirate.glb");
+  return (
+    <>
+      {/* LIGHT */}
+      <mesh
+        geometry={nodes.bakedLight.geometry}
+        scale={0.2}
+        position={[0.42, -0.585, -1.655]}
+      >
+        <meshBasicMaterial color={[2.5 * 8, 2 * 8, 1 * 8]} toneMapped={false} />
+      </mesh>
+      {/* LANTER */}
+      <mesh
+        geometry={nodes.bakedLanter.geometry}
+        scale={0.2}
+        position={[0.42, -0.585, -1.655]}
+      >
+        <meshBasicMaterial
+          color={[7 * 15, 5 * 15, 3 * 15]}
+          toneMapped={false}
+        />
+      </mesh>
+    </>
+  );
 };
 
 export default Light;
