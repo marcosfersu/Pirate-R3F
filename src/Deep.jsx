98e8f8618a9b115252bb7f3bea5e7ec7db7dbbed
Marcos Fernández <marcos.fersu@gmail.com>
2023-11-08 07:24:10 -0300

    undetape chest

--- src/Deep.jsx
+++ src/Deep.jsx
@@ -8,13 +8,37 @@ const Deep = () => {
   return (
     <>
       {/* ANCHOR */}
-
       <mesh
         geometry={nodes.bakedAnchor.geometry}
         position={[-1.5, -0.5, 1]}
         rotation-y={Math.PI * 0.8}
       >
         <meshBasicMaterial map={anchorBaked} />
+      </mesh>
+
+      {/* CHEST TOP*/}
+      <mesh
+        geometry={nodes.chestTop.geometry}
+        position={[2, -6.65, 0]}
+        rotation-y={Math.PI * -0.5}
+        rotation-z={Math.PI * -0.5}
+      >
+        <meshBasicMaterial map={anchorBaked} />
+      </mesh>
+
+      {/* CHEST BUTTON*/}
+      <mesh
+        geometry={nodes.chestButton.geometry}
+        position={[2, -7, 0]}
+        rotation-y={Math.PI * -0.5}
+      >
+        <meshBasicMaterial map={anchorBaked} />
+      </mesh>
+
+      {/* FLOOR */}
+      <mesh rotation-x={-Math.PI * 0.5} position={[2, -7.19, 0]}>
+        <planeGeometry args={[15, 15, 1, 1]} />
+        <meshBasicMaterial color={"#3D322C"} />
       </mesh>
     </>
   );
